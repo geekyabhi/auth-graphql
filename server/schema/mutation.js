48fa2b89c85bfa665e996c52a1ad688d8280970a
Abhinav Singh <thakurabhinav17122001@gmail.com>
2021-04-04 12:53:27 +0530

    Login Logout Authenticated checked

--- server/schema/mutation.js
+++ server/schema/mutation.js
@@ -13,7 +13,25 @@ const mutation=new GraphQLObjectType({
                 password:{type:GraphQLString}
             },
             resolve(parentValue,{email,password},req){
-                AuthService.signup({email,password,req})
+                return AuthService.signup({email,password,req})
+            }
+        },
+        logout:{
+            type:UserType,
+            resolve(parentValue,args,req){
+                const {user}=req
+                req.logout()
+                return user
+            }
+        },
+        login:{
+            type:UserType,
+            args:{
+                email:{type:GraphQLString},
+                password:{type:GraphQLString}
+            },
+            resolve(parentValue,{email,password},req){
+                return AuthService.login({email,password,req})
             }
         }
     }
